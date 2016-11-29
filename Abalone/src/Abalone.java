7debf15aca0d2ae74d7bea5be09624e1a48613cb
Julian Rengstorf <julian.rengstorf@nordakademie.de>
2016-11-29 14:24:15 +0000

    initial file

---
+++ Abalone/src/Abalone.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Abalone extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Abalone Game Team Markus&Julian");
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
