af9e73b1307ed0b85790ef83b09c47d3c908fff6
Markus Jaehn <markus.jaehn@nordakademie.de>
2017-01-07 14:42:41 +0000

    Tidied up GameLogic

--- Abalone/src/GameLogic.java
+++ Abalone/src/GameLogic.java
@@ -2,16 +2,68 @@ import java.util.ArrayList;
 
 
 public class GameLogic {
+	
+	// FIELDS
 
 	private PieceType currentPlayer;
 	private Cell[][] cells = new Cell[11][11];
 	private ArrayList<Cell> selectedCells = new ArrayList<Cell>();
 
 	private ArrayList<Cell> selectableCells = new ArrayList<Cell>();
+	
+	// CONSTRCTOR
 
 	public GameLogic(Cell[][] cells) {
 		this.cells = cells;
 	}
+	
+	// GETTER / SETTER
+	
+	public ArrayList<Cell> getSelectedCells() {
+		return selectedCells;
+	}
+	
+	public void setSelectedCells(ArrayList<Cell> selectedCells) {
+		this.selectedCells = selectedCells;
+	}
+	
+	public int getNumberOfSelectedCells() {
+		return selectedCells.size();
+	}
+	
+	public boolean isSelectable(Cell cell) {
+		return selectableCells.contains(cell) && (selectedCells.size() < 3);
+	}
+	
+	public void setSelectable(Cell cell) {
+		selectableCells.add(cell);
+	}
+	
+	public void emptySelectableCells() {
+		selectableCells = new ArrayList<Cell>();
+	}
+	
+	public boolean isSelected(Cell cell) {
+		return selectedCells.contains(cell);
+	}
+	
+	public void setSelected(Cell cell) {
+		selectedCells.add(cell);
+	}
+	
+	public void deSelect(Cell cell) {
+		selectedCells.remove(cell);
+	}
+	
+	public void setCurrentPlayer(PieceType player) {
+		this.currentPlayer = player;
+	}
+	
+	public PieceType getCurrentPlayer() {
+		return currentPlayer;
+	}
+	
+	// METHODS
 
 	public void initializeSelectable() {
 		for (Cell[] line : cells) {
@@ -22,49 +74,6 @@ public class GameLogic {
 		}
 	}
 
-	public ArrayList<Cell> getSelectedCells() {
-		return selectedCells;
-	}
-
-	public void setSelectedCells(ArrayList<Cell> selectedCells) {
-		this.selectedCells = selectedCells;
-	}
-
-	public int getNumberOfSelectedCells() {
-		return selectedCells.size();
-	}
-
-	public boolean isSelectable(Cell cell) {
-		return selectableCells.contains(cell) && (selectedCells.size() < 3);
-	}
-
-	public void setSelectable(Cell cell) {
-		selectableCells.add(cell);
-	}
-
-	public void emptySelectableCells() {
-		selectableCells = new ArrayList<Cell>();
-	}
-
-	public boolean isSelected(Cell cell) {
-		return selectedCells.contains(cell);
-	}
-
-	public void setSelected(Cell cell) {
-		selectedCells.add(cell);
-	}
-
-	public void deSelect(Cell cell) {
-		selectedCells.remove(cell);
-	}
-
-	public void setCurrentPlayer(PieceType player) {
-		this.currentPlayer = player;
-	}
-
-	public PieceType getCurrentPlayer() {
-		return currentPlayer;
-	}
 
 	public void findAllNeighbours() {
 
@@ -134,12 +143,6 @@ public class GameLogic {
 				return;
 			}
 		}
-		/*
-		 * for (Cell cell : selectedCells) { cell = null; //
-		 * cell.setxLocation(cell.getxLocation()+deltaX); //
-		 * cell.setyLocation(cell.getyLocation()+deltaY); }
-		 */
-
 		
 		if (selectedCells.size()==1||inLane(travelCell)) {
 			swapPiecesInLane(travelCell);
